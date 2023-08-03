8e6f9755aacbea4e355f05870705e4adea347694
M Baki <69335110+mbaki98@users.noreply.github.com>
2023-08-03 10:54:51 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,18 @@
+//create web server
+//create a web server and listen on port 3000
+//http://localhost:3000/
+const express = require('express');
+const app = express();
+//get the comments module
+const comments = require('./comments.js');
+
+//serve static files from public folder
+app.use(express.static('public'));
+
+//use the comments module
+app.use('/comments', comments);
+
+//start the server and listen on port 3000
+app.listen(3000, () => {
+  console.log('Server started at port 3000');
+});
